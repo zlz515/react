92fda31eb2768f3bb7a3eeb85328979266387405
zlz515 <65152854+zlz515@users.noreply.github.com>
2020-05-15 18:56:31 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Admin, Resource} from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { PostList, PostEdit, PostCreate} from './posts';
+import { UserList } from './users';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
+
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} />
+    </Admin>
+);
+
+export default App;
